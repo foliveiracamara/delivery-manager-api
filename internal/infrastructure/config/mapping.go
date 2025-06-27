8006b509f54a0263b589c03515530a3116ae00cc
Felipe Camara <felipe.camara@example.com>
2025-06-27 02:08:13 -0300

    chore: simplifying config

--- internal/infrastructure/config/mapping.go
+++ internal/infrastructure/config/mapping.go
@@ -1,25 +1,15 @@
 package config
 
 type Config struct {
-	App      App      `mapstructure:"app"`
-	Server   Server   `mapstructure:"server"`
-	Database Database `mapstructure:"database"`
+	App    App    `mapstructure:"app"`
+	Server Server `mapstructure:"server"`
 }
 
 type App struct {
 	Name        string `mapstructure:"name"`
-	Debug       bool   `mapstructure:"debug"`
 	Environment string `mapstructure:"environment"`
 }
 
 type Server struct {
 	Port int `mapstructure:"port"`
-}
-
-type Database struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	User     string `mapstructure:"user"`
-	Password string `mapstructure:"password"`
-	Database string `mapstructure:"database"`
 }
